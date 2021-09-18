068172d3312f5ddf2aea6e4a5cb34f4ef8dfd13b
ichigo-dev <ichigo@web-freework.com>
2021-09-19 00:26:48 +0900

    Implemented users_controller

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,15 @@
+class UsersController < ApplicationController
+  def show
+    user = User.find(params[:id])
+    render json: user
+  end
+
+  def create
+    user = User.new(name: params[:name])
+    if user.save
+      render json: {}
+    else
+      render json: {err: user.errors}
+    end
+  end
+end
